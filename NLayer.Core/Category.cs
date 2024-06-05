95a046532bb274fb59676752bce8e6e27d39374f
Deniz Doğru <deniz.dogru@venhancer.com>
2024-06-05 14:44:35 +0300

    Add project files.

---
+++ NLayer.Core/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace NLayer.Core;
+
+public class Category : BaseEntity
+{
+    public string Name { get; set; }
+
+    // Category'nin artık birden çok products'ı var.
+    // referans verdiğimiz propertylere navigation property denir.
+    public ICollection<Product> Products { get; set; }
+
+}
---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+    IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+    //Tolist, ToListAsync gibi metodları çağrırken döner
+    // productRepository.where(x => x.Id>5).OrderBy.ToListAsync();
+    IQueryable<T> Where(Expression<Func<T, bool>> expression);
+    IQueryable<bool> AnyAsync(Expression<Func<T, bool>> expression);
+    Task AddAsync(T entity);
+    Task AddRangeAsync(IEnumerable<T> entities);
+    // Update işleminde sadece state değiştiği için asenkron bir işlem olmasına gerek yoktur.
+    void Update(T entity);
+    void Delete(T entity);
+    void RemoveRange(IEnumerable<T> entities);
+
+
+}
