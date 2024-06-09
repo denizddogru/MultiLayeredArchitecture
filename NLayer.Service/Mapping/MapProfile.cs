78b0a85fea752c8400966bea1368e6eb98399571
Deniz Doğru <deniz.dogru@venhancer.com>
2024-06-10 01:12:32 +0300

    entity,dto conversionları için business(servis) katmanına automapper kütüphanesi eklendi

---
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+
+namespace NLayer.Service.Mapping;
+
+public class MapProfile : Profile
+{
+    public MapProfile()
+    {
+        // product'ı productmap'e dönüştürdük
+        CreateMap<Product, ProductDto>().ReverseMap();
+        CreateMap<Category, CategoryDto>().ReverseMap();
+        CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
+        CreateMap<ProductUpdateDto, Product>();
+
+    }
+}
